36f1dee0a07899d77540e9f8324c59c0dae13387
Simonono <simon.larsson@student.tabyenskilda.se>
2022-05-10 21:25:02 +0200

    v19

--- script.js
+++ script.js
@@ -22,3 +22,17 @@ window.onclick = function(event) {
 
 // Dropdown end
 
+//Mobile dropdown start
+
+function myFunction() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "block";
+    }
+}
+
+//Mobile dropdown end
+
+
